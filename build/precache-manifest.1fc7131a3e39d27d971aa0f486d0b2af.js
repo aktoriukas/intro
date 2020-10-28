2fc81e74efc54323f9fca6d6df369504e16e6d2d
aktoriukas <gediminas.strumila@gmail.com>
2020-10-28 11:06:01 +0000

    add build

---
+++ build/precache-manifest.1fc7131a3e39d27d971aa0f486d0b2af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b01c0499d70d6c67e00d1f7517c494c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2ee535686d1bcabe1be6",
+    "url": "./static/css/main.6ea1adb3.chunk.css"
+  },
+  {
+    "revision": "fcf6360832449fdea698",
+    "url": "./static/js/2.aff2666f.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "./static/js/2.aff2666f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ee535686d1bcabe1be6",
+    "url": "./static/js/main.3475e2ae.chunk.js"
+  },
+  {
+    "revision": "45ae7fd8a4f9e628a733",
+    "url": "./static/js/runtime-main.94e5cee8.js"
+  },
+  {
+    "revision": "12b47f077a3923d36497c0f721bbd193",
+    "url": "./static/media/wall.12b47f07.jpg"
+  }
+]);
