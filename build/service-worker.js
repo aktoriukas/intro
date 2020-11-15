d5041b511d68b7f0f1b9b69bb4af6c5fdb38f67e
aktoriukas <gediminas.strumila@gmail.com>
2020-11-15 19:27:59 +0000

    safari bug fixed

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0ba11b054cd75e9ad1fcde1a5f051378.js"
+  "./precache-manifest.aac33705852e7dec37db531956413387.js"
 );
 
 self.addEventListener('message', (event) => {
